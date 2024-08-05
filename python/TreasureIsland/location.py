456a243b69595a8ead9e967a64d83fa6a78ca78b
AmitWalaH <algoworks818@gmail.com>
2024-08-05 21:59:57 +0530

    created treasure map and introduction to our Python project

---
+++ python/TreasureIsland/location.py
@@ -0,0 +1,23 @@
+
+
+
+
+
+island_map = {
+    "beach": {
+        "description": "You are on a sandy beach. You see a path leading into the jungle.",
+        "actions": {"go north": "jungle"}
+    },
+    "jungle": {
+        "description": "You are in a dense jungle. The trees are tall and the undergrowth is thick. There is a cave to the east and ancient ruins to the west.",
+        "actions": {"go south": "beach", "go east": "cave"}
+    },
+    "cave": {
+        "description": "You are in a dark, damp cave. There is a faint light coming from deeper within.",
+        "actions": {"go west": "jungle", "go deeper": "treasure chamber"}
+    },
+    "treasure chamber": {
+        "description": "You have found the hidden treasure chamber! Gold and jewels are piled high. You have achieved your goal!",
+        "actions": {"go back": "cave"}
+    }
+}
---
+++ python/TreasureIsland/main.py
@@ -0,0 +1,7 @@
+print("\t\t\t\t\t\t\t\t\tWelcome to the Treasure Island Adventure Game!")
+print("Rumors speak of a legendary treasure is hidden somewhere on this island.")
+print("Your goal is to navigate through the island, solve puzzles, and uncover the hidden treasure.")
+print("Explore different locations on the island, collect items, and solve puzzles to find clues that will lead you to the treasure.")
+
+def navigation(choice):
+    
