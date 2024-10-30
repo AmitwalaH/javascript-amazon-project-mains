f2d4eb477c29a1ada12049e44b42dbf16cdf2993
AmitWalaH <algoworks818@gmail.com>
2024-10-30 14:42:48 +0530

    converts products into classes

--- javascript-amazon-project-main/javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/javascript-amazon-project-main/data/products.js
@@ -1,4 +1,6 @@
-export function getProduct(productId){
+import { formatCurrency } from "../scripts/utils/money.js";
+
+export function getProduct(productId) {
   let matchingProduct;
 
   products.forEach((product) => {
@@ -6,8 +8,86 @@ export function getProduct(productId){
       matchingProduct = product;
     }
   });
+
   return matchingProduct;
 }
+
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice() {
+    return `$${formatCurrency(this.priceCents)}`;
+  }
+
+  extraInfoHTML() {
+    return "";
+  }
+}
+
+class Clothing extends Product {
+  sizeChartLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() {
+    // super.extraInfoHTML();
+    return `
+      <a href="${this.sizeChartLink}" target="_blank">
+        Size chart
+      </a>
+    `;
+  }
+}
+
+/*
+const date = new Date();
+console.log(date);
+console.log(date.toLocaleTimeString());
+*/
+
+/*
+console.log(this);
+
+const object2 = {
+  a: 2,
+  b: this.a
+};
+*/
+
+/*
+function logThis() {
+  console.log(this);
+}
+logThis();
+logThis.call('hello');
+
+this
+const object3 = {
+  method: () => {
+    console.log(this);
+  }
+};
+object3.method();
+*/
 
 export const products = [
   {
@@ -480,24 +560,9 @@ export const products = [
     priceCents: 2400,
     keywords: ["sweaters", "hoodies", "apparel", "mens"],
   },
-  {
-    id: "id1",
-    image: "images/products/backpack.jpg",
-    name: "Black Backpack",
-    rating: {
-      stars: 4.5,
-      count: 123,
-    },
-    priceCents: 2500,
-  },
-  {
-    id: "id2",
-    image: "images/products/umbrella.jpg",
-    name: "Large Green Umbrella",
-    rating: {
-      stars: 5,
-      count: 456,
-    },
-    priceCents: 2999,
-  },
-];
+].map((productDetails) => {
+  if (productDetails.type === "clothing") {
+    return new Clothing(productDetails);
+  }
+  return new Product(productDetails);
+});
