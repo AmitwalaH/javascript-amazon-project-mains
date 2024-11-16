14aa1a2cf568a84c7dc8072a9f033625de80bb67
AmitWalaH <algoworks818@gmail.com>
2024-11-16 14:31:53 +0530

    Downloaded Express package

--- Jokester/index.js
+++
@@ -1,6 +0,0 @@
-const jokes = require('give-me-a-joke');
-const punycode = require('punycode');
-const colors = require('colors');
-jokes.getRandomDadJoke(function (joke){
-    console.log(joke.rainbow);    
-})
